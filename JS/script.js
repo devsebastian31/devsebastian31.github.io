793d09c860649e60ef3baa4dd2e5f928a0cfe94d
devsebastian31 <zhunaulasebastian31@gmail.com>
2024-10-15 20:20:35 -0500

    Version 1

--- JS/script.js
+++ JS/script.js
@@ -17,6 +17,36 @@ window.onload = function() {
 
 
 
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+
+
+
 // Espera a que el documento HTML se cargue completamente antes de ejecutar el código JavaScript
 document.addEventListener('DOMContentLoaded', function () {
 
